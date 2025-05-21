4d6e3f457e7c72a50c7e41f8664e2f8e018f764b
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2025-05-21 17:13:53 +0900

    가장 큰 수

---
+++ 김한결/가장 큰 수.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution 
+{
+    public string solution(int[] numbers) 
+    {
+        string[] temp = new string[numbers.Length];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            temp[i] = numbers[i].ToString();
+        }
+
+        Array.Sort(temp, (a, b) => (b + a).CompareTo(a + b));
+
+        if (temp[0] == "0")
+            return "0";
+        else
+            return string.Concat(temp);
+    }
+}
