3edf41525c1ac8e5cbd293df82305ec50d3eb735
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2025-05-22 17:13:46 +0900

    모의고사

---
+++ 김한결/모의고사.cs
@@ -0,0 +1,28 @@
+public int[] solution(int[] answers) 
+{
+int[] number1 = {1, 2, 3, 4, 5};
+int[] number2 = {2, 1, 2, 3, 2, 4, 2, 5};
+int[] number3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+int[] correct = {0, 0, 0};
+
+List<int> answer = new List<int>();
+
+for(int i = 0; i < answers.Length; i++)
+{
+    if(number1[i % 5] == answers[i]) correct[0] ++;
+    if(number2[i % 8] == answers[i]) correct[1] ++;
+    if(number3[i % 10] == answers[i]) correct[2] ++;
+}
+
+int maxValue = correct.Max();
+
+for(int i = 0; i < 3; i++)
+{
+    if(maxValue == correct[i]) 
+    {
+        answer.Add(i + 1);
+    }
+}
+return answer.ToArray();
+}
